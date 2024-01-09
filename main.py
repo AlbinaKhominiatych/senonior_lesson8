ed454306122efe61e61725551ed38271674d423e
khominiatych <hominyatichalbina@gmail.com>
2024-01-09 17:19:30 +0200

    запис у файл log з прикладом помилки

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import logging
 #запис у файл
 logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode = "w")
 logging.debug("debug")
-logging.info("Інформація про рядок коду")
-logging.warning("Попередження про можливу помилку")
-logging.error("Помилка в програмі")
-logging.critical("Критична помилка")
+logging.info("info")
+logging.warning("warning")
+logging.error("error")
+logging.critical("critical")
 
 try:
     print(10/0)
