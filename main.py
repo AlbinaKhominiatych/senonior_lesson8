6d46fb38051aabc39070e395895cdf6fa69c5d67
khominiatych <hominyatichalbina@gmail.com>
2024-01-09 17:13:04 +0200

    запис у файл log

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #логування - вивід помилок через бібліотеку
 import logging
-logging.basicConfig(level=logging.DEBUG)
+#запис у файл
+logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w")
 logging.debug("debug")
 logging.info("Інформація про рядок коду")
-logging.warning("Попередження про можливу помилку")
-logging.error("Помилка в програмі")
-logging.critical("Критична помилка")
